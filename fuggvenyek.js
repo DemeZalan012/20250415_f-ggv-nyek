4d484c2387c283c2c87cc23ff9a9771dac85360f
Deme Zalán <DemeZalan@pg.local>
2025-04-15 10:13:36 +0200

    java done 1. feladat

---
+++ fuggvenyek.js
@@ -0,0 +1,13 @@
+//function add (a, b) {return a + b;}
+
+//console.log(add(3, 2));
+
+let add = (a, b) => a + b;
+console.log(add(5, 3))
+
+let nev = prompt("Adja meg a nevét!");
+(function() {let nev = "Zalán", udv = function()
+    {return "Csa" + nev + "vagyok";}
+
+console.log(udv());}());
+console.log(nev)
